d0873450f60daa8573df6874640c9aad05659447
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-21 07:54:01 -0500

    Serverless Function Setup

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,9 +19,9 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://mirjSolution.github.io/meet/'],
+  redirect_uris: ['https://mirjsolution.github.io/meet/'],
   javascript_origins: [
-    'https://mirjSolution.github.io',
+    'https://mirjsolution.github.io',
     'http://localhost:3000',
   ],
 };
