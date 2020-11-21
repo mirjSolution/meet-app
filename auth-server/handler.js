f1cdfe301cf9a72012e7ca5d9919a4cae9c08e68
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-21 06:44:39 -0500

    Serverless Function Setup

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -93,7 +93,10 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
+          // "Access-Control-Allow-Headers": "Content-Type",
           'Access-Control-Allow-Origin': '*',
+          // "Access-Control-Allow-Credentials": true,
+          // "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
         },
         body: JSON.stringify(token),
       };
