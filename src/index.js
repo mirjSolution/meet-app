8aeaaf73df963b762611c46e4ac5cde41db9c5bd
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-27 13:55:45 -0500

    Continous Delivery

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+// import { notify } from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +17,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.config('09c879730ef74076b38bef72a6b338f4').install();
+// notify();
+// notify(new Error('Test Atatus Setup'));
